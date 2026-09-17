d5dd7610c9537b5a415d3ac8724bda29ced56144
Messs-prog <mesiahgail@gmail.com>
2026-09-17 10:11:46 +0800

    Update Tusoktusok_balingkilat_Panahon.py

--- q1/Tusoktusok_balingkilat_Panahon.py
+++ q1/Tusoktusok_balingkilat_Panahon.py
@@ -1,1 +1,25 @@
+class Sauce:
+  def
+    __init__(self, name, taste):
+    self.name = name
+    self.taste = taste
+    print(self.name, "sauce is ready and it tastes", self.taste)
+  def __str__(self):
+    return "Here is the " + self.taste + ""+ self.name
+
+class Tusoktusok:
+  def
+    __init__(self, name, sauce):
+    self.name = name
+    self.sauce = sauce
+    print(self.name, "dipped in", self.sauce.name)
+  def eat(self):
+    print("Eating", self.name, "with", self.sauce.name)
+    def __del__(self):
+    print("I threw the", self.name, "in the trash can")
+
+vinegar = Sauce("vinegar", "sour")
+fishball Tusoktusok("fishball", vinegar)
+fishball.eat()
+del fishball
 
